ec93951fcf42c85e1b77d8b8968d6001345cf820
Paul Moore <p.f.moore@gmail.com>
2013-12-03 21:32:03 +0000

    Read the __init__.py file directly to get the package version

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
 from setuptools import setup, find_packages
-import sample
+import os
+import re
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Read the version number from a source file.
+# Code taken from pip's setup.py
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+# Get the long description from the relevant file
 with open('DESCRIPTION.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name="sample",
-    version=sample.__version__,
+    version=find_version('sample', '__init__.py'),
     description="A sample Python project",
     long_description=long_description,
 
