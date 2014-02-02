6cd77025722b729c96e2168689394c3fe12cf30d
Alexis Huet <alexis.huet.python@gmail.com>
2014-02-02 15:23:05 -0300

    Add .pylintrc and clean code accordingly.
    To use it :
    $ pip install pylint
    $ pylint --rcfile=.pylintrc sample

--- sample/__init__.py
+++ sample/__init__.py
@@ -1,3 +1,8 @@
+# coding: utf-8
+"""
+This module is a sample containing an HelloWorld like class and associated
+unit test.
+"""
 __version__ = '0.1'
 
 
--- sample/tests/__init__.py
+++ sample/tests/__init__.py
@@ -1,13 +1,22 @@
 # coding: utf-8
+"""Helpers for unit tests."""
 from unittest import TestCase
 
 
+#pylint: disable=R0904
+# TestCase23: Too many public methods (48/20)
 class TestCase23(TestCase):
+    """
+    This class let you use the new unittest API (>=3.2),
+    even if you're running 2.7
+    """
 
     # Monkey-patch for Python 2/3 compatibility inspired from
     # http://people.canonical.com/~cjwatson/\
     # bzr/cdimage/mainline/lib/cdimage/tests/helpers.py
     if not hasattr(TestCase, 'assertCountEqual'):
+        #pylint: disable=E1101
+        # E1101=Class 'TestCase' has no 'assertItemsEqual' member
         assertCountEqual = TestCase.assertItemsEqual
     if not hasattr(TestCase, 'assertRegex'):
         assertRegex = TestCase.assertRegexpMatches
