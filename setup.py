73d6bd0c37ae5e83c54e46260c54d0b494e06d4e
Marcus Smith <qwcode@gmail.com>
2014-02-26 22:57:43 -0800

    add missing comma

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed.
-    install_requires = ['SomeDependency']
+    install_requires = ['SomeDependency'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
