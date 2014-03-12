9b109b4522789762ad9dcd10540872133b6126e1
Marcus Smith <qwcode@gmail.com>
2014-03-11 21:55:48 -0700

    add data_files example

--- setup.py
+++ setup.py
@@ -86,6 +86,12 @@ setup(
         'sample': ['package_data.dat'],
     },
 
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[('my_data', ['data/data_file'])],
+
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
