d2acf9ea2b99ade23a8b1b4be2084807b518ce81
Matt Iversen <teh.ivo@gmail.com>
2014-05-25 22:53:04 +1000

    Clean up setup.py imports, add notes

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup, find_packages
-import codecs
-import os
-import re
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
 
-here = os.path.abspath(os.path.dirname(__file__))
+here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with codecs.open(os.path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -20,7 +19,7 @@ setup(
     description="A sample Python project",
     long_description=long_description,
 
-    # The project URL.
+    # The project's main homepage.
     url='https://github.com/pypa/sampleproject',
 
     # Author details
@@ -30,6 +29,7 @@ setup(
     # Choose your license
     license='MIT',
 
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -50,16 +50,16 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 
     # What does your project relate to?
     keywords='sample setuptools development',
 
     # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages.
+    # simple. Or you can use find_packages().
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
 
     # List run-time dependencies here.  These will be installed by pip when your
