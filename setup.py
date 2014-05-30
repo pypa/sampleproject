f034b8981128e930b4a5d15b32ccf7ea97f1b880
Lars Storjord <larsstor@ifi.uio.no>
2014-05-30 19:17:14 +0200

    Consistently use ' quotes.
    The old version mixed ' and " quotes. This change
    consistently uses ', which were in the majority
    before.

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name="sample",
+    name='sample',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
     version='1.2.0',
 
-    description="A sample Python project",
+    description='A sample Python project',
     long_description=long_description,
 
     # The project's main homepage.
@@ -60,7 +60,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
