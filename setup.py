e7de7861963274595c5b3b0d6942e35ce5a733bc
Carl Meyer <carl@oddbird.net>
2015-09-28 11:03:35 -0600

    Pull long-description directly from README.rst.

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
