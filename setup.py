16be69b2ed725ce5d54b2e3487442fe5d2529622
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-10-14 19:21:27 +0300

    Make it a comment.

--- setup.py
+++ setup.py
@@ -71,8 +71,9 @@ setup(
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     
-    # Alternatively, if you want to distribute just a my_module.py
-    py_modules=["my_module"],
+    # Alternatively, if you want to distribute just a my_module.py, uncomment 
+    # this:
+    #   py_modules=["my_module"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
