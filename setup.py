9be24e3a5686e48b60ec3ad90de7eee3c113c4e4
Ester Ramos <ester.ramos@ktech.com>
2018-10-27 11:01:33 +0100

    Add Python37 support to setup.py and tox.ini

--- setup.py
+++ setup.py
@@ -111,6 +111,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # This field adds keywords for your project which will appear on the
