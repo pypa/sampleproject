279890f40f95d10ba6902baeddb84afe46453221
Jirka Borovec <Borda@users.noreply.github.com>
2019-05-14 23:17:34 +0200

    update docs (#1)
    * update docs
    * update CI

--- sandbox/__init__.py
+++ sandbox/__init__.py
@@ -1,5 +1,7 @@
 __version__ = '0.1.2'
 __doc__ = """
+# Sample project
+
 A sample project that exists as an aid to the Python Packaging User Guide's Tutorial
  on Packaging and Distributing Projects.
 
