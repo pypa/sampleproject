f89336e88d2f5d70c0fa4e40e44a7fabbf3c917a
Dustin Ingram <di@users.noreply.github.com>
2019-11-04 14:28:16 -0600

    Add Python 3.8 classifier

--- setup.py
+++ setup.py
@@ -113,6 +113,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     # This field adds keywords for your project which will appear on the
