cecab2119a195c5e6c732e13379e0e52a676fba8
Dustin Ingram <di@users.noreply.github.com>
2019-11-04 14:29:17 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.3.0',  # Required
+    version='1.3.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
