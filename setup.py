204c774755209d0a0eff8a1ae825f5efb3508cff
Jiri Borovec <jirka.borovec@seznam.cz>
2020-01-24 23:58:53 +0100

    update setup

--- setup.py
+++ setup.py
@@ -45,12 +45,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.3.1',  # Required
+    version=sandbox.__version__,  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='A sample Python project',  # Optional
+    description=sandbox.__doc__,  # Optional
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
