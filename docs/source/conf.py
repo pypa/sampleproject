246eadfb79c4bd6149c1e667fb6d024bc7b41a52
J. Borovec <jirka.borovec@seznam.cz>
2020-04-14 21:30:12 +0200

    strict & fixing Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ with open('intro.md', 'w') as fp:
 
 # If your documentation needs a minimal Sphinx version, state it here.
 
-needs_sphinx = '1.4'
+needs_sphinx = '1.8'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -81,7 +81,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['modules.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
@@ -103,7 +103,7 @@ html_theme = 'classic'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []  # '_static'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
