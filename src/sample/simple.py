c7a4cfb287b680e3b63977fd6ed9d461a100d0f6
Ryan Long <ryanlong1004@gmail.com>
2020-06-14 11:38:04 -0400

    26 added simple module and test module

---
+++ src/sample/simple.py
@@ -0,0 +1,14 @@
+def add_one(number):
+    """Adds one to number
+
+    Args:
+        number (int/float):
+
+    Returns:
+        number
+
+    Raises
+    """
+    return number + 1
+
+
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,6 +2,16 @@
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
 
+import unittest
 
-def test_success():
-    assert True
+from sample.simple import add_one
+
+
+class TestSimple(unittest.TestCase):
+
+    def test_add_one(self):
+        self.assertEqual(add_one(5), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
