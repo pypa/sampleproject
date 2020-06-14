772b5bc8f82a9cc9081e92c65f8133d85e223e38
Ryan Long <ryanlong1004@gmail.com>
2020-06-14 11:22:04 -0400

    106 add commas to keywords in setup.py

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
     # project page. What does your project relate to?
     #
     # Note that this is a string of words separated by whitespace, not a list.
-    keywords='sample setuptools development',  # Optional
+    keywords='sample, setuptools, development',  # Optional
 
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
