3b0207a3396b634e41af20e82e0c710194d5f5dd
Dustin Ingram <di@users.noreply.github.com>
2020-06-25 14:08:53 -0500

    Version 2.0.0 (#125)

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.3.1',  # Required
+    version='2.0.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
