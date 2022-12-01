db5806e0a3204034c51b1c00dde7d5eb3fa2532e
Paul Moore <p.f.moore@gmail.com>
2022-12-01 16:53:46 +0000

    Remove mention of deprecated setuptools features (#181)

--- setup.py
+++ setup.py
@@ -145,18 +145,7 @@ setup(
     package_data={  # Optional
         "sample": ["package_data.dat"],
     },
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[("my_data", ["data/data_file"])],  # Optional
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
+    # Entry points. The following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         "console_scripts": [
