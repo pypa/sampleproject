43c64dd9ca46b10068a91e0b05b6841e0d291d03
Paul Moore <p.f.moore@gmail.com>
2022-12-01 17:19:08 +0000

    Merge main and fix up comments

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,2 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support an example that excludes installing
-# the "tests" package
+# The inclusion of the tests module is not meant to offer best practices for
+# testing in general.
