dca046fd0c5f1f151930ae60cd15029b699227a0
NagaPrasad-AA <nagaprasad.kondaboina@aa.com>
2024-09-08 18:01:58 -0500

    Update test.tf

--- dir1/test.tf
+++ dir1/test.tf
@@ -1,12 +1,30 @@
-resource "google_storage_bucket_iam_member" "test" {
-  bucket = "test"
-  role   = "roles/viewer"
-
-  # This is bad
-  member = "allUsers"
+provider "aws" {
+  region = "us-west-2"
 }
 
-# testtttt
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
 
-# test
-# test
+  # This will flag because public access is allowed
+  # hi
+  acl = "public-read"
+}
+
+resource "aws_security_group" "example" {
+  name_prefix = "example-sg-"
+
+  # This will flag because it allows ingress on port 22 from any IP
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
